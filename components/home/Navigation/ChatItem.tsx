f4534ee9580dee9af7d14ddb1305d72368c3bf1a
mmy <15224924822@163.com>
2023-11-30 22:06:31 +0800

    edit and delete chat in Client and DB

--- components/home/Navigation/ChatItem.tsx
+++ components/home/Navigation/ChatItem.tsx
@@ -3,6 +3,9 @@ import { MdCheck, MdClose, MdDeleteOutline } from "react-icons/md";
 import { PiChatBold, PiTrashBold } from "react-icons/pi";
 import { AiOutlineEdit } from "react-icons/ai";
 import { useEffect, useState } from "react";
+import { useEventBusContext } from "@/components/EventBusContext";
+import { useAppContext } from "@/components/AppContext";
+import { ActionType } from "@/reducers/AppReducer";
 type Props = {
   item: Chat,
   selected: boolean,
@@ -17,6 +20,55 @@ export default function ChatItem({ item, selected, onSelected}: Props) {
     setEditing(false);
     setDeleting(false);
   }, [selected])
+
+  /* 修改对话标题 */
+  const { publish } = useEventBusContext();
+  // 临时保存对话标题
+  const [title, setTitle] = useState(item.title);
+  const updateChat = async () => {
+    const response = await fetch('/api/chat/update', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({id: item.id, title})
+    })
+    if (!response.ok) {
+      console.log(response.statusText);
+      return 
+    }
+    const {code} = await response.json();
+    // 如果数据修改成功，那么通过事件通知来更新本地的chatList
+    if (code===0) {
+      publish('fetchChatList');
+    }
+  }
+
+  /* 删除对话 */
+  const {dispatch} = useAppContext();
+  const deleteChat = async () => {
+    const response = await fetch(`/api/chat/delete?id=${item.id}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+    if (!response.ok) {
+      console.log(response.statusText);
+      return 
+    }
+    const {code} = await response.json();
+    // 如果数据修改成功，那么通过事件通知来更新本地的chatList
+    if (code===0) {
+      publish('fetchChatList');
+      // 删除当前选中的chat之后，应该将客户端的selectedChat置空
+      dispatch({
+        type: ActionType.UPDATE,
+        field: 'selectedChat',
+        value: null
+      })
+    }
+  }
   return <li
     key={item.id}
     className={`relative group flex items-center p-3 space-x-3 cursor-pointer rounded-md hover:bg-gray-800 
@@ -31,7 +83,11 @@ export default function ChatItem({ item, selected, onSelected}: Props) {
     {editing ? 
       <input 
         className="flex-1 min-w-0 bg-transparent outline-none"
-        defaultValue={item.title}
+        // react中手动实现mvvm
+        value={title}
+        onChange={(e) => {
+          setTitle(e.target.value);
+        }}
         autoFocus={true}
       /> : 
       <div className="relative flex-1 whitespace-nowrap overflow-hidden">
@@ -49,18 +105,22 @@ export default function ChatItem({ item, selected, onSelected}: Props) {
     {selected 
       && <div className="absolute right-1 flex">
         {
-          editing || deleting ? <>
+          editing || deleting ? 
+          <>
             <button 
               className="p-1 hover:text-white"
               onClick={(e) => {
                 if (deleting) {
+                  deleteChat();
                   console.log('delete');
                 } else {
+                  // 保存编辑内容的时候发送网络请求
+                  // 更改数据库中相应元素的状态
+                  updateChat()
                   console.log('edit');
                 }
                 setDeleting(false);
                 setEditing(false);
-                e.stopPropagation();
               }}
             >
               <MdCheck />
@@ -74,7 +134,8 @@ export default function ChatItem({ item, selected, onSelected}: Props) {
             >
               <MdClose />
             </div> 
-          </>: <>
+          </> : 
+          <>
             <button 
               className="p-1 hover:text-white"
               onClick={(e) => {
